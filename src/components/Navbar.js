a92424be452a24b96264bcc9e9bd193850209b74
Jaswant Singh <113183496+JaswantSingh007@users.noreply.github.com>
2023-02-13 23:35:40 +0530

    Implemented Navbar Component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,17 @@
 import { Link } from "react-router-dom";
+import '../index.css';
 
 function Navbar() {
     return (
-      <div>
-        Navbar
+      <div id="nav">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/create-post">Create Post</Link>
+          </li>
+        </ul>
       </div>
     );
   }
