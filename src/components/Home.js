ebf728e72d5cf520168342bdfab67b500d4df296
Jaswant Singh <113183496+JaswantSingh007@users.noreply.github.com>
2023-02-27 22:16:47 +0530

    Creating Post Detail component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom";
 import { firestore } from "../firebase";
 
 function Home() {
+  // getting posts from firestore
+  // storeing data from firestore using useState hooks
   const[posts, setPosts]=useState([]);
-
+  
+  // logic for getting the data from firestore we will use the useEffect to get the data from firestore
   useEffect(()=>{
     firestore
       .collection('posts')
@@ -27,11 +30,13 @@ function Home() {
         <h1>Tech Blog</h1>
         <div id="blog-by">Jaswant Singh</div>
 
+        {/* we have posts in array so, Looping over post */}
+        {/* this function will give me post and index of the array */}
         {posts.map((post, index)=>{
           return(
-            <div className="post" key={`post-${index}`}>
-              <Link to={`/post/${post.id}`}>
-                <h3>{post.title}</h3>
+            <div className="post" key={`post-${index}`}>  {/* simply rendering the post using div */}
+              <Link to={`/post/${post.id}`}>  {/* we are linking this back to the postDetail page */}
+                <h3>{post.title}</h3>  {/* getting title of the post */}
               </Link>
               <p>{post.subTitle}</p>
             </div>
--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,9 +1,29 @@
-function PostDetail() {
-    return (
-      <div className="">
-        PostDetail
-      </div>
-    );
-  }
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { firestore } from "../firebase";
 
-  export default PostDetail;
+function PostDetail() {
+  const [post, setPost] = useState({});
+  // getting the post id
+  const { postId } = useParams();
+
+  useEffect(()=>{
+    firestore
+      .collection('posts')
+      .doc(postId)   //getting the document with post id
+      .get()
+      .then((snapshot)=>{
+        console.log('snapshot',snapshot.data());
+        setPost(snapshot.data());
+      });
+  }, [postId]);
+
+  return (
+    <div className="post-detail">
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+    </div>
+  );
+}
+
+export default PostDetail;
